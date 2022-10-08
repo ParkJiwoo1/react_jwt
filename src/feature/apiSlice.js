6230d090b0fe720ebc519575e0bc0305a6cb09ee
Jiwoo Park <jiwoo9446@naver.com>
2022-10-09 00:09:16 +0900

    redux toolkit auth

--- src/feature/apiSlice.js
+++ src/feature/apiSlice.js
@@ -12,3 +12,26 @@ const baseQuery = fetchBaseQuery({
     return headers;
   },
 });
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.originalStatus === 403) {
+    console.log("sending refresh token");
+    const refreshResult = await baseQuery("/refresh", api, extraOptions);
+    console.log(refreshResult);
+    if (refreshResult?.data) {
+      const user = api.getState().auth.user;
+      api.dispatch(setCredentials({ ...refreshResult.data, user }));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryWithReauth,
+  endpoint: (builder) => ({}),
+});
