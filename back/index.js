fec4629ae05033c92b2929f8b28d8cadb3513301
Jiwoo Park <jiwoo9446@naver.com>
2022-10-11 17:10:11 +0900

    generate refresh token

--- back/index.js
+++ back/index.js
@@ -17,20 +17,52 @@ const users = [
   },
 ];
 
+let refreshTokens = [];
+
+app.post("/api/refresh", (req, res) => {
+  const refreshToken = req.body.token;
+  if (!refreshToken) return res.status(401).json("not authenticated");
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json("refresh token is invalid");
+  }
+  jwt.verify(refreshToken, "myRefreshKey", (err, user) => {
+    err && console.log(err);
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+
+    res.status(200).json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  });
+});
+
+const generateAccessToken = (user) => {
+  return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "tokenKey", {
+    expiresIn: "15m",
+  });
+};
+const generateRefreshToken = (user) => {
+  return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "myRefreshKey", {});
+};
+
 app.post("/api/login", (req, res) => {
   const { username, pwd } = req.body;
   const user = users.find((x) => {
     return x.username === username && x.pwd === pwd;
   });
   if (user) {
-    const accessToken = jwt.sign(
-      { id: user.id, isAdmin: user.isAdmin },
-      "tokenKey"
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
     res.json({
       username: user.username,
       isAdmin: user.isAdmin,
       accessToken,
+      refreshToken,
     });
   } else {
     res.status(400).json("username or password incorrect");
@@ -59,5 +91,10 @@ app.delete("/api/users/:userId", verify, (req, res) => {
   } else {
     req.status(403).json("not allowed to delete");
   }
+});
+app.post("/api/logout", verify, (req, res) => {
+  const refreshToken = req.body.token;
+  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+  res.status(200).json("logged out successfully");
 });
 app.listen(5000, () => console.log("back running"));
